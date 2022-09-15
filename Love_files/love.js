1f9d5cde36c8facf8af860f2a75eebf93cabdb84
(千~葉)GeraTear <40741618+Geratear@users.noreply.github.com>
2022-09-15 13:15:28 +0800

    Update love.js

--- Love_files/love.js
+++ Love_files/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 楷体,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("芸芸，我爱你", 23, 16);
+            ctx.fillText("未来的她，我爱你", 23, 16);
             ctx.restore();
         },
         clear: function() {
